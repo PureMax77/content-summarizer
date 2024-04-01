6eef2e7bc6b37cb7e55f60c138bcbe07f39f1612
ertr <ertr@coinplug.com>
2024-04-01 09:02:46 +0900

    :fix: 메인페이지 css 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,17 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <MainButton
-        content={"YouTube 검색 요약"}
-        onClick={() => router.push(PagePath.SEARCH)}
-      />
-      <MainButton
-        content={"YouTube URL 요약"}
-        onClick={() => router.push(PagePath.DIRECTURL)}
-      />
+    <div className="flex justify-center items-center w-screen">
+      <div className="flex justify-center items-center mt-60">
+        <MainButton
+          content={"YouTube 검색 요약"}
+          onClick={() => router.push(PagePath.SEARCH)}
+        />
+        <MainButton
+          content={"YouTube URL 요약"}
+          onClick={() => router.push(PagePath.DIRECTURL)}
+        />
+      </div>
     </div>
   );
 }
